05fcd4c30f5729a155af20c5ec1bb2920f6852bd
Ab Momin <mominurrahman10748@gmail.com>
2025-12-22 23:20:15 +0600

    more updated

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("contestHub_db");
     const contestCollections = db.collection("contests");
